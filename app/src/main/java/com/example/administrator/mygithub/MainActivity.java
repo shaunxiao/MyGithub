365881a080a5fa02c9e0b082014aa6658eb8230b
shaun <xiaoqizcf@gmail.com>
2016-07-05 14:39:22 +0800

    [code]xxx

--- app/src/main/java/com/example/administrator/mygithub/MainActivity.java
+++ app/src/main/java/com/example/administrator/mygithub/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.mygithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity
         extends AppCompatActivity
@@ -11,5 +12,8 @@ public class MainActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "hi", Toast.LENGTH_SHORT)
+             .show();
     }
 }
